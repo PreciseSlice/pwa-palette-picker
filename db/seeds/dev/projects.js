cbe2cbe590feddf0406f3a6152e263ea857c3788
Spencer Herms <slherms@gmail.com>
2018-03-14 15:41:33 -0600

    set up seed file

---
+++ db/seeds/dev/projects.js
@@ -0,0 +1,36 @@
+exports.seed = function(knex, Promise) {
+  return knex('palettes').del()
+    .then(() => knex('projects').del())
+    .then(() => {
+      return Promise.all([
+        knex('projects').insert({
+          name: 'first project',
+        }, 'id')
+        .then(project => {
+          return knex('palettes').insert([
+            {
+              project_id: project[0],
+              palette_name: 'pallet one',
+              color_one: '#848f5b',
+              color_two: '#21fe01',
+              color_three: '#76789c',
+              color_four: '#6c63d3',
+              color_five: '#f0f759'
+            },
+            {
+              project_id: project[0],
+              palette_name: 'pallet two',
+              color_one: '#847f5c',
+              color_two: '#51fe21',
+              color_three: '#96285c',
+              color_four: '#1c66d3',
+              color_five: '#f2f621'
+            }
+          ])
+        })
+        .then(() => console.log('Seeding complete!'))
+        .catch(error => console.log(`Error seeding data: ${error}`))
+      ])
+    })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
