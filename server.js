803706bc34853306a70b9192cf2dfc0d14930602
Spencer Herms <slherms@gmail.com>
2018-03-15 00:31:05 -0600

    refactor app endpoints to interact with database

--- server.js
+++ server.js
@@ -8,93 +8,92 @@ const database = require('knex')(configuration);
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 app.use(bodyParser.json());
-
-app.locals.projects = [{ projectId: 1, projectName: 'first project' }];
-app.locals.palettes = [
-  {
-    project_id: 1,
-    palette_id: 1,
-    palette_name: 'pallet one',
-    color_one: '#848f5b',
-    color_two: '#21fe01',
-    color_three: '#76789c',
-    color_four: '#6c63d3',
-    color_five: '#f0f759'
-  },
-  {
-    project_id: 1,
-    palette_id: 2,
-    palette_name: 'pallet two',
-    color_one: '#847f5c',
-    color_two: '#51fe21',
-    color_three: '#96285c',
-    color_four: '#1c66d3',
-    color_five: '#f2f621'
-  }
-];
-
 app.use(express.static('public'));
-
 app.listen(app.get('port'), () => {
   console.log(
     `${app.locals.title} sever is running on port ${app.get('port')}.`
   );
 });
 
-app.get('/api/v1/projects/:projectId', (request, response) => {
-  const { projectId } = request.params;
-  const requestId = parseInt(projectId);
-  const project = app.locals.projects.filter(project => project.projectId === requestId)
-  const palette = app.locals.palettes.filter(palette => palette.projectId === requestId);
-
-  if (project && palette) {
-    return response.status(200).json({project, palette});
-  } else {
-    return response.sendStatus(404);
-  }
+app.get('/api/v1/projects/:id', (request, response) => {
+  database('projects')
+    .where('id', request.params.id)
+    .select()
+    .then(project => {
+      if (project.length) {
+        response.status(200).json(project);
+      } else {
+        response.status(404).json({
+          error: `Could not find project with id ${request.params.id}`
+        });
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
 });
 
 app.get('/api/v1/palettes/:id', (request, response) => {
-  const { id } = request.params;
-  const requestId = parseInt(id);
-  const palette = app.locals.palettes.filter(palette => palette.paletteId === requestId);
-
-  if (palette) {
-    return response.status(200).json(palette);
-  } else {
-    return response.sendStatus(404);
-  }
+  database('palettes')
+    .where('id', request.params.id)
+    .select()
+    .then(palette => {
+      if (palette.length) {
+        response.status(200).json(palette);
+      } else {
+        response.status(404).json({
+          error: `Could not find palette with id ${request.params.id}`
+        });
+      }
+    });
 });
 
+// should both projects and pallets be handled in the same post
 app.post('/api/v1/projects/', (request, response) => {
-  const { paletteName, projectName, colors } = request.body;
-  const projectId = app.locals.projects.length + 1;
-  const paletteId = app.locals.palettes.length + 1;
+  const { project, palette } = request.body;
 
-  if (projectName) {
-    app.locals.projects.push({ projectId, projectName });
-    app.locals.palettes.push({ paletteName, colors })
-    response.status(201).json({ projectId, paletteId, projectName, colors });
-  } else {
-    return response.status(422).send({
-      error: 'No name property provided'
-    });
+  for (let requiredParameter of ['name']) {
+    if (!project[requiredParameter]) {
+      return response.status(422).send({
+        error: `Expected format: { name: <String> }. 
+                You're missing a "${requiredParameter}" property.`
+      });
+    }
   }
+
+  database('projects')
+    .insert(project, 'id')
+    // then database('palettes').insert(palette, 'id')
+    .then(project => {
+      response.status(201).json({ id: project[0] });
+    })
+    .catch(error => {
+      response.status(500).json({ error });
+    });
+
+  // this gets the info in the database however I cannot
+  // do anything after the first responce
+  // database('palettes').insert(palette, 'id')
+  // .then(project => {
+  //   response.status(201).json({ id: palette[0] })
+  // })
+  // .catch(error => {
+  //   response.status(500).json({ error });
+  // });
 });
 
 app.delete('/api/v1/palettes/:id', (request, response) => {
-  const { id } = request.params;
-  const requestId = parseInt(id);
-
-  if (requestId) {
-    const itemToDelete = app.locals.palettes.find( palette => palette.paletteId === requestId)
-    const index = app.locals.palettes.indexOf(itemToDelete);
-    console.log(index);
-    app.locals.palettes.slice(index, 1);
-    return response.status(202).json(itemToDelete);
-  } else {
-    return response.status(422).send({
-      error: 'No paletteId property provided'
+  database('palettes')
+    .where('id', request.params.id)
+    .select()
+    .del()
+    .then(palette => {
+      if (palette) {
+        response.status(202).json(request.body);
+      } else {
+        return response.status(422).send({
+          error: 'No paletteId property provided'
+        });
+      }
     });
-  }
 });
