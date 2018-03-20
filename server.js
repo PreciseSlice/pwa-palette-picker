20d77a7910baae3214d4190a9d3158e62142359d
Spencer Herms <slherms@gmail.com>
2018-03-20 10:18:56 -0600

    add redirect functionality

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
+const http = express.createServer();
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
@@ -14,6 +15,12 @@ app.listen(app.get('port'), () => {
     `${app.locals.title} sever is running on port ${app.get('port')}.`
   );
 });
+
+http.get('*', function(req, res) {  
+    res.redirect('https://' + req.headers.host + req.url);
+})
+
+http.listen(8080);
 
 app.get('/api/v1/projects', (request, response) => {
   database('projects')
